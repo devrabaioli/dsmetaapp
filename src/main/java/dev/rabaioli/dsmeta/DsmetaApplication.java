bf8f5a0ec62ef715b8c986ef58604eb30817dd2c
devrabaioli <devrabaioli@gmail.com>
2023-01-12 14:49:51 +0000

    Twilio SMS

--- src/main/java/dev/rabaioli/dsmeta/DsmetaApplication.java
+++ src/main/java/dev/rabaioli/dsmeta/DsmetaApplication.java
@@ -1,6 +1,10 @@
 package dev.rabaioli.dsmeta;
 
 import org.springframework.boot.SpringApplication;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -10,4 +14,16 @@ public class DsmetaApplication {
 		SpringApplication.run(DsmetaApplication.class, args);
 	}
 
+	@Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector ajpConnector = new Connector("AJP/1.3");
+        ajpConnector.setPort(9090);
+        ajpConnector.setSecure(false);
+        ajpConnector.setAllowTrace(false);
+        ajpConnector.setScheme("http");
+       ((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
+    tomcat.addAdditionalTomcatConnectors(ajpConnector);
+    return tomcat;
+    }
 }
--- src/main/java/dev/rabaioli/dsmeta/resources/SaleResource.java
+++ src/main/java/dev/rabaioli/dsmeta/resources/SaleResource.java
@@ -4,7 +4,8 @@ package dev.rabaioli.dsmeta.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import dev.rabaioli.dsmeta.entities.Sale;
 import dev.rabaioli.dsmeta.services.SaleService;
+import dev.rabaioli.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value="/sales")
@@ -20,6 +22,8 @@ public class SaleResource {
 	@Autowired
 	private SaleService service;
 	
+	@Autowired
+	private SmsService smsService;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public Page<Sale> findSales(
@@ -28,6 +32,11 @@ public class SaleResource {
 			Pageable page
 			){
 		return service.findPages(minDate, maxDate, page);
+	}
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 
 }
